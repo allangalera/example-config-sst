bbd40a994aa0833f91e546484a0fc4101a798444
Allan Galera <allangalera@gmail.com>
2023-05-18 00:16:42 -0300

    revert to mimic the way I had the initial problem

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,7 +1,7 @@
-// import { Config } from 'sst/node/config';
+import { Config } from 'sst/node/config';
 
 export const makeApiCall = async () => {
-	// console.log(Config.MY_EXAMPLE_CONFIG);
+	console.log(Config.MY_EXAMPLE_CONFIG);
 
 	return 'just some data';
 };
